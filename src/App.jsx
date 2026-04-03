56fa19aa7ab250afa064c93f6409ba97209ecd55
Ignacio <ignacionazar9@gmail.com>
2026-04-03 13:31:39 -0300

    agregue la libreria de react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
   return (
     <>
       <h1>Hola mundo</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
