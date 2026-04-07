cd107c62f809c6ec1975df9009e388081e195a7a
Ignacio <ignacionazar9@gmail.com>
2026-04-07 12:42:33 -0300

    agregue la logica necesaria para proteger las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -11,25 +11,43 @@ import Formulario from "./components/pages/Formulario";
 import Error404 from "./components/pages/Error404";
 import Login from "./components/pages/Login";
 import { useState } from "react";
-
+import ProtectorAdmin from "./components/routes/ProtectorAdmin";
 
 function App() {
-  const usuarioLogueado = JSON.parse(sessionStorage.getItem("userKey")) || false;
-  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado)
+  const usuarioLogueado =
+    JSON.parse(sessionStorage.getItem("userKey")) || false;
+  const [usuarioAdmin, setUsuarioAdmin] = useState(usuarioLogueado);
 
   return (
     <BrowserRouter>
-      <Menu usuarioAdmin={usuarioAdmin} setUsuarioAdmin={setUsuarioAdmin}/>
+      <Menu usuarioAdmin={usuarioAdmin} setUsuarioAdmin={setUsuarioAdmin} />
       <main>
-      <Routes>
-        <Route path="/" element={<Inicio></Inicio>}></Route>
-        <Route path="/detalle" element={<DetalleVehiculo></DetalleVehiculo>}></Route>
-        <Route path="/login" element={<Login setUsuarioAdmin={setUsuarioAdmin}></Login>}></Route>
-         <Route path="/administrador" element={<Administrador></Administrador>}></Route>
-         <Route path="/administrador/crear" element={<Formulario></Formulario>}></Route>
-         <Route path="/administrador/editar" element={<Formulario></Formulario>}></Route>
-         <Route path="*" element={<Error404></Error404>}></Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Inicio></Inicio>}></Route>
+          <Route
+            path="/detalle"
+            element={<DetalleVehiculo></DetalleVehiculo>}
+          ></Route>
+          <Route
+            path="/login"
+            element={<Login setUsuarioAdmin={setUsuarioAdmin}></Login>}
+          ></Route>
+          <Route
+            path="/administrador"
+            element={<ProtectorAdmin isAdmin={usuarioAdmin}></ProtectorAdmin>}
+          >
+            <Route index element={<Administrador></Administrador>}></Route>
+            <Route
+              path="crear"
+              element={<Formulario></Formulario>}
+            ></Route>
+            <Route
+              path="editar"
+              element={<Formulario></Formulario>}
+            ></Route>
+          </Route>
+          <Route path="*" element={<Error404></Error404>}></Route>
+        </Routes>
       </main>
       <Footer />
     </BrowserRouter>
---
+++ src/components/routes/ProtectorAdmin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router';
+
+const ProtectorAdmin = ({isAdmin}) => {
+    //si no soy adm
+    if(!isAdmin){
+        return <Navigate to={"/"}></Navigate>
+    }
+
+    //si soy administrador muestro las rutas
+    return <Outlet></Outlet>
+};
+
+export default ProtectorAdmin;
