e5d2d6a7336c1158cda19f693105186af2d0f216
Ignacio <ignacionazar9@gmail.com>
2026-06-07 20:24:18 -0300

    agregue mapa de google maps en el componente Contacto

--- src/components/shared/Contacto.jsx
+++ src/components/shared/Contacto.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../pages/vehiculo/Contacto.css";
 
 const Contacto = () => {
   return (
@@ -32,6 +33,16 @@ const Contacto = () => {
               <li className="mb-1">Tucumán, Argentina</li>
             </ul>
           </div>
+        </div>
+        <div className="mt-5 rounded overflow-hidden maps">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3560.4802989513896!2d-65.2342316253447!3d-26.82467178952945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225c5ddae62d37%3A0x6678f7eb778713dc!2sLazarte%20Automoviles!5e0!3m2!1ses!2sar!4v1780874070504!5m2!1ses!2sar"
+            width="100%"
+            height="100%"
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </div>
     </section>
