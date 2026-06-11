d2eb8abefb5ab2a6e0152d26deff85d66f11d699
Ignacio <ignacionazar9@gmail.com>
2026-06-11 01:03:45 -0300

    agregue modal de imagenes en el componente DetalleVehiculo

--- src/components/pages/DetalleVehiculo.jsx
+++ src/components/pages/DetalleVehiculo.jsx
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Container, Card, Row, Col, Carousel } from "react-bootstrap";
+import { Container, Card, Row, Col, Carousel, Modal } from "react-bootstrap";
 import { useParams } from "react-router";
 import { leerVehiculoPorId } from "../../../helpers/queries";
-import "./vehiculo/CardVehiculo.css"
+import "./vehiculo/CardVehiculo.css";
+import "./vehiculo/Modal.css";
 
 const DetalleVehiculo = () => {
   const { id } = useParams();
   const [vehiculo, setVehiculo] = useState(null);
+
+  const [showModal, setShowModal] = useState(false);
+
+  const [indexFoto, setIndexFoto] = useState(0);
 
   useEffect(() => {
     obtenerVehiculo();
@@ -22,20 +27,29 @@ const DetalleVehiculo = () => {
     }
   }
 
+  const handleSelect = (selectedIndex) => {
+    setIndexFoto(selectedIndex);
+  };
+
   return (
     <Container className="my-3 mainSection">
       <Card>
         <Row>
           <Col md={6}>
             {vehiculo.imagenes?.length > 0 ? (
-              <Carousel>
+              <Carousel activeIndex={indexFoto} onSelect={handleSelect}>
                 {vehiculo.imagenes.map((img, index) => (
                   <Carousel.Item key={index}>
                     <img
                       src={img}
                       className="d-block w-100"
-                      style={{ height: "400px", objectFit: "cover" }}
+                      style={{
+                        height: "400px",
+                        objectFit: "cover",
+                        cursor: "zoom-in",
+                      }}
                       alt={`imagen-${index}`}
+                      onClick={() => setShowModal(true)}
                     />
                   </Carousel.Item>
                 ))}
@@ -91,6 +105,54 @@ const DetalleVehiculo = () => {
           </Col>
         </Row>
       </Card>
+
+      <Modal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        fullscreen={true}
+        centered
+        contentClassName="bg-transparent border-0 m-0 p-0"
+      >
+        <button
+          type="button"
+          className="btn-close btn-close-white position-absolute top-0 end-0 m-4 fs-4"
+          style={{ zIndex: 1050 }}
+          onClick={() => setShowModal(false)}
+          aria-label="Close"
+        ></button>
+
+        <div
+          className="position-absolute top-0 start-0 m-4 text-light primary-font fs-5"
+          style={{ zIndex: 1050 }}
+        >
+          {indexFoto + 1} / {vehiculo.imagenes?.length}
+        </div>
+
+        <Modal.Body
+          className="p-0 vh-100 w-100"
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0.85)",
+            backdropFilter: "blur(5px)",
+          }}
+        >
+          <Carousel
+            activeIndex={indexFoto}
+            onSelect={handleSelect}
+            interval={null}
+            className="w-100 h-100"
+          >
+            {vehiculo.imagenes?.map((img, index) => (
+              <Carousel.Item key={index} className="h-100 text-center">
+                <img
+                  src={img}
+                  alt={`Zoom-${index}`}
+                  className="img-fluid modal-imagen-ml"
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 };
