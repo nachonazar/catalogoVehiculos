51c99460b303cd7ca3a26b6f77f6cff0b70678bd
Ignacio <ignacionazar9@gmail.com>
2026-08-10 18:13:59 -0300

    fix: corregi error de build typeError manualChunks en Vite 8

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ["react", "react-dom", "react-router-dom"],
-          ui: ["sweetalert2"],
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("sweetalert2")) {
+              return "ui";
+            }
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router")
+            ) {
+              return "vendor";
+            }
+          }
         },
       },
     },
