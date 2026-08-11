93d030ced3568aaf5403da5db22b4a73376c04f4
Ignacio <ignacionazar9@gmail.com>
2026-08-11 13:00:39 -0300

    fix

--- src/components/pages/LayoutAdmin.jsx
+++ src/components/pages/LayoutAdmin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../../assets/logo-transparente.png";
+import logo from "../../assets/logo-transparente.webp";
 import { useTheme } from "../../context/ThemeContext";
 
 const LayoutAdmin = ({ titulo, headerExtra, children }) => {
